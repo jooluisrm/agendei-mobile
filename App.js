e0c3fdd1f87c8c4622a4d03b4d182cddc6809268
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-03 23:34:07 -0300

    tela profile feita

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import Login from "./src/screens/login/login.jsx";
 import Account from "./src/screens/account/account.jsx";
 import AbaHome from "./src/screens/abaHome/abaHome.jsx";
 import AbaCalendar from "./src/screens/abaCalendar/abaCalendar.jsx";
+import AbaProfile from "./src/screens/abaProfile/abaProfile.jsx";
 
 function App() {
 
     return <>
-        <AbaCalendar />
+        <AbaProfile />
     </>;
 }
 
